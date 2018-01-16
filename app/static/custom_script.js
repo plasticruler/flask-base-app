c669466aa94348e5ac64efc4e704746ef6beda4d
marmite@dollar <mailbox@a20.co.za>
2018-01-16 07:52:39 +0200

    draw inline sparklines of coin closing price for last 60 days when requesting price

--- app/static/custom_script.js
+++ app/static/custom_script.js
@@ -17,19 +17,37 @@ function processPriceData(d,e){
   console.log(e);
   $(e).html('USD:' + d.USD);  
 }
-function getPrice(code, target)
-{
+function getPrice(code, coin_id)
+{  
+
   $.ajax({
           dataType:"json",
           url:`https://min-api.cryptocompare.com/data/price?fsym=${code}&tsyms=USD,EUR`
         }
   ).done((d,t)=>{
+
                   if (d.Type==1)
                   {
-                    $(target).html(d.Message);
+                    $('#price_' + coin_id).text('Error!');
                     return;
                   }
-                  $(target).html('$' + d.USD);
+                  $('#price_' + coin_id).text('$' + d.USD);
                 }
  );
+ $.ajax({
+             dataType:"json",
+             url:`https://min-api.cryptocompare.com/data/histoday?fsym=${code}&tsym=USD&limit=60`
+        }
+        ).done((data,status)=>{
+                                values = [];
+                                chrt = $('#chart_'+coin_id).peity("line", {width:64});
+                                
+                                for (var j=0; j< data.Data.length;j++){
+                                      console.log(data.Data[j]);
+                                      values.push(data.Data[j].close);
+                                }
+                                chrt.text(values.join(","))
+                                    .change()
+                              });
+ 
 }
